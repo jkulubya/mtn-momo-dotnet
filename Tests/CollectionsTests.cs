9331c80167279c74a0c19351d6377577fe03144d
JK <jameskitamirike@gmail.com>
2019-03-13 13:29:31 +0300

    Add more tests

--- Tests/CollectionsTests.cs
+++ Tests/CollectionsTests.cs
@@ -1,16 +1,203 @@
 using System;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http.Testing;
 using Xunit;
 using MomoApi.NET;
 
 namespace Tests
 {
-    public class UnitTest1
+    public class CollectionsTests : BaseTestClass
     {
+        private const string SubscriptionKey = "d484a1f0d34f4301916d0f2c9e9106a2";
+        private const string TokenPath = "/collection/token/";
+
         [Fact]
-        public void Test1()
+        public void ThrowsOnInvalidConfig()
         {
-            var x = new Momo();
-            var y = x.Collections;
+            var momo = new Momo(new MomoConfig());
+            var result = Record.Exception(() =>
+            {
+                var collections = momo.Collections;
+            });
+
+            Assert.NotNull(result);
+            Assert.IsType<ArgumentException>(result);
+        }
+
+        [Fact]
+        public async Task GetBalanceSendsProperRequest()
+        {
+            using (var httpTest = new HttpTest())
+            {
+                httpTest
+                    .RespondWithJson(new
+                    {
+                        access_token = AccessToken,
+                        expires_in = 3600,
+                        token_type = "access_token"
+                    })
+                    .RespondWithJson(new
+                    {
+                        availableBalance = "20000",
+                        currency = "UGX"
+                    });
+                var config = new MomoConfig();
+                config.UserId = UserId;
+                config.UserSecret = UserSecretKey;
+                config.SubscriptionKeys.Collections = SubscriptionKey;
+                
+                var momo = new Momo(config);
+                var collections = momo.Collections;
+
+                var result = await collections.GetBalance();
+
+                httpTest.ShouldHaveCalled(BaseUri.AppendPathSegment(TokenPath));
+                httpTest.ShouldHaveCalled(BaseUri.AppendPathSegment("/collection/v1_0/account/balance"))
+                    .WithHeader("Authorization", $"Bearer {AccessToken}")
+                    .WithHeader("Ocp-Apim-Subscription-Key", SubscriptionKey)
+                    .WithHeader("X-Target-Environment", "sandbox");
+                
+                Assert.Equal(20000, result.AvailableBalance);
+                Assert.Equal("UGX", result.Currency);
+            }
+        }
+
+        [Fact]
+        public async Task CheckIfPayerIsActiveSendsProperRequest()
+        {
+            using (var httpTest = new HttpTest())
+            {
+                httpTest
+                    .RespondWithJson(new
+                    {
+                        access_token = AccessToken,
+                        expires_in = 3600,
+                        token_type = "access_token"
+                    })
+                    .RespondWithJson(new {result = true});
+                var config = new MomoConfig();
+                config.UserId = UserId;
+                config.UserSecret = UserSecretKey;
+                config.SubscriptionKeys.Collections = SubscriptionKey;
+                
+                var momo = new Momo(config);
+                var collections = momo.Collections;
+
+                var result = await collections.IsAccountHolderActive(new Party("0777000000", PartyIdType.Msisdn));
+
+                httpTest.ShouldHaveCalled(BaseUri.AppendPathSegment(TokenPath));
+                httpTest.ShouldHaveCalled(
+                        BaseUri.AppendPathSegment($"/collection/v1_0/accountholder/msisdn/0777000000/active"))
+                    .WithHeader("Authorization", $"Bearer {AccessToken}")
+                    .WithHeader("Ocp-Apim-Subscription-Key", SubscriptionKey)
+                    .WithHeader("X-Target-Environment", "sandbox");
+                Assert.True(result);
+            }
+        }
+
+        [Fact]
+        public async Task RequestToPaySendsProperRequest()
+        {
+            using (var httpTest = new HttpTest())
+            {
+                httpTest
+                    .RespondWithJson(new
+                    {
+                        access_token = AccessToken,
+                        expires_in = 3600,
+                        token_type = "access_token"
+                    })
+                    .RespondWithJson(new
+                    {
+                        amount= "25000",
+                        currency = "UGX"
+                    }, 202);
+                
+                var config = new MomoConfig();
+                config.UserId = UserId;
+                config.UserSecret = UserSecretKey;
+                config.SubscriptionKeys.Collections = SubscriptionKey;
+                
+                var momo  =  new Momo(config);
+                var collections = momo.Collections;
+
+                var result = await collections.RequestToPay(
+                    25000.00M,
+                    "UGX",
+                    "XX",
+                    new Party("0777000000", PartyIdType.Msisdn),
+                    "YY",
+                    "ZZ",
+                    new Uri("http://www.example.com")
+                );
+
+                httpTest.ShouldHaveCalled(BaseUri.AppendPathSegment(TokenPath));
+                httpTest.ShouldHaveCalled(
+                        BaseUri.AppendPathSegment("/collection/v1_0/requesttopay"))
+                    .WithHeader("Authorization", $"Bearer {AccessToken}")
+                    .WithHeader("X-Reference-Id", result.ToString())
+                    .WithHeader("X-Target-Environment", "sandbox")
+                    .WithHeader("Ocp-Apim-Subscription-Key", SubscriptionKey)
+                    .WithRequestJson(new
+                    {
+                        
+                    });
+            }
+        }
+
+        [Fact]
+        public async Task GetTransactionSendsProperRequest()
+        {
+            using (var httpTest = new HttpTest())
+            {
+                httpTest
+                    .RespondWithJson(new
+                    {
+                        access_token = AccessToken,
+                        expires_in = 3600,
+                        token_type = "access_token"
+                    })
+                    .RespondWithJson(new
+                    {
+                        amount = "25000.00",
+                        currency = "UGX",
+                        financialTransactionId ="XXXX",
+                        externalid = "YYYY",
+                        payer = new
+                        {
+                            partyIdType =  "MSISDN",
+                            partyId = "0777000000"
+                        }
+                    });
+
+                var config = new MomoConfig();
+                config.UserId = UserId;
+                config.UserSecret = UserSecretKey;
+                config.SubscriptionKeys.Collections = SubscriptionKey;
+
+                var momo = new Momo(config);
+                var collections = momo.Collections;
+
+                var guid = Guid.NewGuid();
+                
+                var result = await collections.GetTransaction(guid);
+                
+                httpTest.ShouldHaveCalled(BaseUri.AppendPathSegment(TokenPath));
+                httpTest.ShouldHaveCalled(
+                        BaseUri.AppendPathSegment("/collection/v1_0/requesttopay"))
+                    .WithHeader("Authorization", $"Bearer {AccessToken}")
+                    .WithHeader("X-Target-Environment", "sandbox")
+                    .WithHeader("Ocp-Apim-Subscription-Key", SubscriptionKey);
+                
+                Assert.Equal(25000M, result.Amount);
+                Assert.Equal("UGX", result.Currency);
+                Assert.Equal("XXXX", result.FinancialTransactionId);
+                Assert.Equal("YYYY", result.ExternalId);
+                Assert.NotNull(result.Payer);
+                Assert.Equal(PartyIdType.Msisdn, result.Payer.PartyIdType);
+                Assert.Equal("0777000000", result.Payer.PartyId);
+            }
         }
     }
 }
---
+++ Tests/ErrorTests.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Threading.Tasks;
+using Flurl.Http.Testing;
+using MomoApi.NET;
+using MomoApi.NET.Exceptions;
+using Xunit;
+
+namespace Tests
+{
+    public class ErrorTests : BaseTestClass
+    {
+        private const string SubscriptionKey = "d484a1f0d34f4301916d0f2c9e9106a2";
+        private const string TokenPath = "/collection/token/";
+        
+        [Fact]
+        public async Task NetworkErrorIsThrownWhenInvalidResponseIsReceived()
+        {
+            using (var httpTest = new HttpTest())
+            {
+                httpTest
+                    .RespondWithJson(new
+                    {
+                        access_token = AccessToken,
+                        expires_in = 3600,
+                        token_type = "access_token"
+                    })
+                    .RespondWithJson(new
+                    {
+                        amount = "xx25000.00xx",
+                        currency = "UGX",
+                        financialTransactionId ="XXXX",
+                        externalid = "YYYY",
+                        payer = new
+                        {
+                            partyIdType =  "MSISDN",
+                            partyId = "0777000000"
+                        }
+                    });
+                var config = new MomoConfig();
+                config.UserId = UserId;
+                config.UserSecret = UserSecretKey;
+                config.SubscriptionKeys.Collections = SubscriptionKey;
+
+                var momo = new Momo(config);
+                var collections = momo.Collections;
+
+                var guid = Guid.NewGuid();
+
+                var result = await Record.ExceptionAsync(async () =>
+                {
+                    await collections.GetTransaction(guid);
+                });
+                
+                Assert.NotNull(result);
+                Assert.IsType<NetworkException>(result);
+            }
+        }
+    }
+}
